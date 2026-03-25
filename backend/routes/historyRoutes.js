5c94505436eb7ae7eef0700df43d32ce013a73c1
Nitin Kumar <nitin1112004@gmail.com>
2026-03-25 10:25:16 +0530

    Delete backend directory

--- backend/routes/historyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getLeadHistory } = require('../controllers/historyController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/:leadId', protect, getLeadHistory);
-
-module.exports = router;
